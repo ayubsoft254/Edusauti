42b1ac9e1e7deecc0718f2f4a818869d2017ab24
ayubsoft254 <henryoayub15@gmail.com>
2025-05-13 09:19:37 +0300

    Set up asyncio event loop in manage.py and add production settings for security and database configuration

--- src/manage.py
+++ src/manage.py
@@ -2,11 +2,21 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import asyncio
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    # Set up the event loop
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError as e:
+        if "There is no current event loop in thread" in str(e):
+            asyncio.set_event_loop(asyncio.new_event_loop())
+            loop = asyncio.get_event_loop()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
