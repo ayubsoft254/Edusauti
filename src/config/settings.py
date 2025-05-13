733b45b87d964bb369017f0655db1aafec29699e
ayubsoft254 <henryoayub15@gmail.com>
2025-05-13 16:20:51 +0300

    Add login and logout redirect URLs; update logout button to use form submission

--- src/config/settings.py
+++ src/config/settings.py
@@ -227,3 +227,6 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/dashboard/'
