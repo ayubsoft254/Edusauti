7a7f3e034b318d27c8cca2b616bdea13e1a89f9f
ayubsoft254 <henryoayub15@gmail.com>
2025-06-02 12:57:16 +0300

    Add verbose name and ready method to AiServicesConfig for improved app configuration

--- src/ai_services/apps.py
+++ src/ai_services/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class AiServicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ai_services'
+    verbose_name = 'AI Services'
+    
+    def ready(self):
+        # Import signals when app is ready
+        pass
