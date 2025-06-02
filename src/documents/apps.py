8feaf811e66e593d47679205bb68ecf36a203e88
ayubsoft254 <henryoayub15@gmail.com>
2025-06-02 12:12:51 +0300

    Refactor DocumentsConfig class: ensure signals are imported when the app is ready

--- src/documents/apps.py
+++ src/documents/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class DocumentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'documents'
+    verbose_name = 'Documents'
+    
+    def ready(self):
+        # Import signals when the app is ready
+        import documents.signals
