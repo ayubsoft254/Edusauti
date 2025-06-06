9ef65b9d70e72f885fceaa7559369ef4a77c4360
ayubsoft254 <henryoayub15@gmail.com>
2025-06-06 11:03:33 +0300

    Refactor save_model in DocumentAdmin to delegate file metadata handling to the model's save method

--- src/documents/admin.py
+++ src/documents/admin.py
@@ -73,18 +73,9 @@ class DocumentAdmin(admin.ModelAdmin):
     file_size_mb.short_description = 'File Size (MB)'
     
     def save_model(self, request, obj, form, change):
-        """Override save_model to set file metadata"""
-        if not change and hasattr(obj, 'file') and obj.file:
-            # Set file metadata for new documents
-            obj.original_filename = obj.file.name
-            obj.file_size = obj.file.size
-            
-            # Determine file type from extension
-            if '.' in obj.file.name:
-                extension = obj.file.name.split('.')[-1].lower()
-                obj.file_type = extension
-        
-        # Call parent save_model method
+        """Override save_model to handle admin-specific logic"""
+        # The model's save() method will handle file metadata
+        # Just call parent save_model method
         super(DocumentAdmin, self).save_model(request, obj, form, change)
 
 
