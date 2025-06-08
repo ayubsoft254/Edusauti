6fad650f8866f4b560edceab0598865a2f5ab689
ayubsoft254 <henryoayub15@gmail.com>
2025-06-08 14:56:48 +0300

    Refactor database path in production settings: Update SQLite database path to use BASE_DIR for better portability.

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -14,7 +14,8 @@ ALLOWED_HOSTS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/app/data/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
